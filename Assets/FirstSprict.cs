40e94bc4c8531f4ef219fbb307e653fd65fe2917
tomohiroueno <ramune276@gmail.com>
2021-10-08 17:25:25 +0900

    first commit

---
+++ Assets/FirstSprict.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstSprict : MonoBehaviour
+{
+    //  変数定数
+    void Start()
+    {
+        string say = "Hello world"; //代入
+        say = "tokyo";
+        Debug.Log(say);
+
+        const string sayConst = "Hello world2"; //代入
+        Debug.Log(say);
+
+    }
+
+    
+    void Update()
+    {
+    }
+
+}
